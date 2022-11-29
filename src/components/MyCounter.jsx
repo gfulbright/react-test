10f4eaa23b8a9f5d1371124fcb5d0f31d322016d
Greg Fulbright <gregory2fulbright@gmail.com>
2022-11-29 16:48:23 -0700

    call it good

--- src/components/MyCounter.jsx
+++ src/components/MyCounter.jsx
@@ -1,8 +1,15 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 const MyCounter=(props)=> {
     
     const [countState, setCountState] = useState(77)
+
+    useEffect(()=>{
+        if (countState >= 100){
+            alert("Resetting the count back to 77")
+            setCountState(77)
+        }
+    }, [countState])
 
     function incCount() {
         // countState++
